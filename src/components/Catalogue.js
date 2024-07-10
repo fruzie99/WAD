8b2b38d987907ff5d6fc865c07f864e310d36442
Madhurisha V <129486999+fruzie99@users.noreply.github.com>
2024-07-10 11:10:07 +0530

    Update Catalogue.js

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -5,12 +5,12 @@ import { CartContext } from '../context/CartContext'; // Adjust the path as per 
 import './Catalogue.css';
 
 const products = [
-  { id: 1, name: 'Product 1', imageUrl: 'https://images-cdn.ubuy.co.in/635ddb1188b6ae453404b343-unisex-jujutsu-kaisen-anime-gojo-satoru.jpg', description: 'Description 1', price: 10 },
-  { id: 2, name: 'Product 2', imageUrl: 'https://m.media-amazon.com/images/I/61WwFra1CSL._AC_UY1000_.jpg', description: 'Description 2', price: 20 },
-  { id: 3, name: 'Product 3', imageUrl: 'https://images-cdn.ubuy.co.in/6353f77f98449e303f51a0a9-taicanon-mens-3d-naruto-hoodie-cool.jpg', description: 'Description 3', price: 30 },
-  { id: 4, name: 'Product 4', imageUrl: 'https://m.media-amazon.com/images/I/61dmrgeUSHL._AC_UY1100_.jpg', description: 'Description 4', price: 40 },
-  { id: 5, name: 'Product 5', imageUrl: 'https://m.media-amazon.com/images/I/61Lp05LRTEL._AC_UY1000_.jpg', description: 'Description 5', price: 50 },
-  { id: 6, name: 'Product 6', imageUrl: 'https://img.joomcdn.net/676d902a87a0f3b35ab4284e187da8451ffb3127_original.jpeg', description: 'Description 6', price: 60 },
+  { id: 1, name: 'Product 1', imageUrl: 'https://www.rarebeauty.com/products/find-comfort-body-hair-fragrance-mist', description: 'Description 1', price: 10 },
+  { id: 2, name: 'Product 2', imageUrl: 'https://www.rarebeauty.com/products/find-comfort-hydrating-hand-cream', description: 'Description 2', price: 20 },
+  { id: 3, name: 'Product 3', imageUrl: 'https://www.rarebeauty.com/products/soft-pinch-luminous-powder-blush?variant=40970263232647', description: 'Description 3', price: 30 },
+  { id: 4, name: 'Product 4', imageUrl: 'https://www.rarebeauty.com/products/positive-light-liquid-luminizer-mini?variant=40970263429255', description: 'Description 4', price: 40 },
+  { id: 5, name: 'Product 5', imageUrl: 'https://www.rarebeauty.com/products/brow-harmony-flexible-lifting-gel', description: 'Description 5', price: 50 },
+  { id: 6, name: 'Product 6', imageUrl: 'https://www.rarebeauty.com/products/positive-light-silky-touch-highlighter?variant=40253134962823', description: 'Description 6', price: 60 },
   // Add more products as needed
 ];
 
